87cc73b7f5049bd7ebfa66190cbcfa6822637169
putty <minprogramador@gmail.com>
2022-04-16 20:13:15 -0300

    exibindo tarefas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ITasks } from '../interfaces/Tasks';
 
+import styles from './TaskList.module.css';
 interface Props {
-
+    taskList: ITasks[]
 }
 
-const TaskList = ({}:Props) => {
+const TaskList = ({taskList}:Props) => {
     return (
         <>
-        algo
+        {taskList.length > 0 ? taskList.map((task) => (
+            <div key={task.id}>
+                <p>{task.title}</p>
+            </div>
+        )): (<p>Nao ha tarefas cadastradas!</p>)}
         </>
     );
 };
